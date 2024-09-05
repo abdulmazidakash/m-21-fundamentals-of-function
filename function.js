4aaf9c6673dbf8738f4a76b2521592e4e05117a1
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-05 22:10:44 +0600

    21-1 and 21-2 what are function and syntax then function intro with animation

---
+++ function.js
@@ -0,0 +1,19 @@
+const number = 45;
+
+function fanOffKor (){
+	console.log('bosa theke uthe dara');
+	console.log('waik towards the switch');
+	console.log('click the switch to off of the fan');
+}
+
+//function call
+fanOffKor()
+
+function brushTeeth  (){
+	console.log('pick up the brush');
+	console.log('add paste');
+	console.log('2 min goso and goso and fena felo');
+}
+
+//function call
+brushTeeth();
---
+++ functionVisual.js
@@ -0,0 +1,38 @@
+// function sum (x, y) {
+// 	const result = x + y;
+// 	return result;
+// }
+
+// const total = sum (5, 3);
+// console.log(total);
+
+/** 
+function sum (x, y) {
+	return x + y
+}
+
+const name = 'lydia hallie'
+
+let info = {
+	age: 21,
+	nationality: 'dutch',
+
+}
+
+var city = 'san francisco'
+*/
+
+function macherTorkari (mach, shobji, mosla){
+	//cooking instruction
+	console.log('oil doing hot');
+	console.log('mosla burn');
+	console.log('vegetable mixed');
+	console.log(mach + ' din');
+
+	//cooking end
+	return 'delicious fish ready!';
+}
+
+//function call
+const result = macherTorkari ('ellis', 'potato', 'holud');
+console.log(result);
