fbb9e9f05ece5b5d7a765ffd248583514e26abe5
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-06 07:51:07 +0600

    21-6 recap and conditional return of odd and even

--- return.js
+++ return.js
@@ -1,16 +1,16 @@
-function tenTimes (number) {
-	const result  = number * 10;
-	return result;
-}
+// function tenTimes (number) {
+// 	const result  = number * 10;
+// 	return result;
+// }
 
-function cutHalf (number) {
-	const half = number / 2;
-	return half;
-}
+// function cutHalf (number) {
+// 	const half = number / 2;
+// 	return half;
+// }
 
-tenTimes (5);
+// tenTimes (5);
 
-const output = tenTimes (5);
-console.log('output', output);
+// const output = tenTimes (5);
+// console.log('output', output);
 
-const bigNumber = tenTimes (99);
+// const bigNumber = tenTimes (99);
